ee6d05e5c22632cd71285acd6593f1bbcb864bfd
Lapishla IU HPC BigRed200 <davidswygart@gmail.com>
2025-02-01 15:58:45 -0500

    make custom logger

--- sorting/sortSingleRec.py
+++ sorting/sortSingleRec.py
@@ -12,14 +12,16 @@ from pandas import read_csv
 from collections import namedtuple
 from argparse import ArgumentParser
 from subprocess import check_output
-from pprint import pp
+from pprint import pp, pformat
 from git import Repo
 import pickle
+import logging
+log = logging.getLogger(__name__)
 
 def main():
     print('running sortSingleRec.py')
     options = parseInputs()
-    logInfo(options)
+    log = logInfo(options)
     fullRec = loadRecording(recPath=options['paths']['rawData'])
     if options['shortenRec']:
         fullRec = shortenRec(rec=fullRec, timeDur = options['shortenRec'])
@@ -32,15 +34,20 @@ def main():
     saveResults(options)
 
 def logInfo(options):
+    # Save our logger to a custom file
+    log.setLevel(logging.INFO)
+    log.addHandler(logging.FileHandler(options['paths']['myLog']))
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    log.handlers[0].setFormatter(formatter)
+
     from spikeinterface import __version__ as si_version
-    print('SpikeInterface version =', si_version)
-    print('-- Options and paths --')
-    pp(options)
+    log.info(f'SpikeInterface version = {si_version}')
+    log.info(pformat(options))
     
     scriptPath = path.dirname(path.abspath(__file__))
     repo = Repo(scriptPath, search_parent_directories=True)
     sha = repo.head.object.hexsha
-    print('Git Commit = ', sha)
+    log.info(f'Git Commit = {sha}')
 def shortenRec(rec, timeDur):
     tstart = rec._get_t_starts()
     start=tstart[0]
@@ -99,18 +106,20 @@ def getPaths(options):
         paths['probeOutput'].append(path.join(paths['results'], f'probe{i}'))
 
     paths['metaData'] =  path.join(paths['results'], 'jobMetaData')
+    makedirs(paths['metaData'], exist_ok=True)
+    paths['myLog'] = path.join(paths['metaData'], 'sortSingleRec.log')
     ## Specify the expected location of the Slurm log file
     if options['debugWithoutSlurm']:
-        paths['log'] = None
+        paths['slurmLog'] = None
     else:
         logBasename = f'{options['taskID']}_{options['jobID']}.txt' 
-        paths['log'] = path.join(options['jobFolder'], 'logs', logBasename)
+        paths['slurmLog'] = path.join(options['jobFolder'], 'logs', logBasename)
     return paths
 def createProbes(channelMapPath):
     channelMap = read_csv(channelMapPath)
     probes = [] #list of dictionaries
     for probeName, channelInds in channelMap.items():
-        print(f'    creating probe {probeName}')
+        log.info(f'Creating probe {probeName}')
         d = dict()
         d['probeName'] = probeName
         d['probe'] = get_probe(manufacturer='cambridgeneurotech',probe_name=probeName)
@@ -131,14 +140,12 @@ def loadRecording(recPath):
         isSignalChannel = np.char.find(rec.channel_ids, 'CH')==0
         signalChannels = rec.channel_ids[isSignalChannel]
         rec = rec.channel_slice(channel_ids=signalChannels)
-    print('Signal Recording: ',rec)
+    logging.info('Signal Recording: %s', rec)
 
     # Check if recording has multiple segments
     if rec.get_num_segments() > 1:
         segmentDuration = [rec.get_duration(i) for i in range(rec.get_num_segments())]
-        warn('Raw data contains multiple segments with the following time durations (s):'
-            + '\n' + str(segmentDuration)
-            + '\n I will just spike sort the longest segment')
+        log.warning(f'Raw data contains multiple segments with the following time durations (s): {segmentDuration}. Only the longest segment will be spike sorted')
         rec = rec.select_segments(int(np.argmax(segmentDuration)))
     return rec
 def splitRecByProbe(rec,probes):
@@ -151,7 +158,7 @@ def splitRecByProbe(rec,probes):
         firstIndex = lastIndex
     return probes
 def preprocess(rec):
-    print('    preprocessing: ', rec)
+    log.info(f'preprocessing: {rec}')
     rec = bandpass_filter(recording=rec,freq_min=300.,freq_max=3000.)
     bad_channel_ids, _ = detect_bad_channels(rec,method="std",std_mad_threshold=3)
     rec = rec.remove_channels(bad_channel_ids)
@@ -165,7 +172,7 @@ def runSorter(rec,savePath):
     # sorterParameters['nearest_templates'] = rec.get_num_channels()
     #sorterParameters['min_template_size'] = 10 #may need to be decreased for smaller spacing
 
-    print('    sorting: ', rec)
+    log.info(f'sorting: {rec}')
     run_sorter(
         sorter_name='kilosort4',
         recording=rec,
@@ -175,21 +182,17 @@ def runSorter(rec,savePath):
         raise_error=True,
         **sorterParameters)
 def saveResults(options):
-    # Make a folder for metadata
     metaDataFolder = options['paths']['metaData']
-    makedirs(metaDataFolder, exist_ok=True)
 
     # copy settings and log files
     copy2(options['paths']['channelMap'], metaDataFolder)
     copy2(options['paths']['recCsv'], metaDataFolder)
     copy2(options['paths']['batch'] , metaDataFolder)
-    print('Job completed successfully')
+    log.info('Job completed successfully')
     
     # Copy log file to final destination if it exists (no log file if running in debug mode)
-    try: 
-        copy2(options['paths']['log'], metaDataFolder)
-    except:
-        print('No log file found')
+    if options['paths']['slurmLog']: 
+        copy2(options['paths']['slurmLog'], metaDataFolder)
 
 if __name__ == "__main__":
     main()
