84fa2b2df257033097d82a639f5c47c795bbdd73
Lapishla IU HPC BigRed200 <davidswygart@gmail.com>
2025-02-14 16:35:04 -0500

    use built in resample function from SpikeInterface

--- exporting/downSampleRaw.py
+++ exporting/downSampleRaw.py
@@ -2,11 +2,10 @@
 
 from argparse import ArgumentParser
 from spikeinterface.extractors import read_openephys
-from os import listdir, makedirs, path, environ
-from spikeinterface.preprocessing import bandpass_filter
+from os import listdir
 import numpy as np
-import pandas as pd
 import scipy.io
+from spikeinterface.preprocessing import resample
 
 def main():
     print('running downSampleRaw.py')
@@ -15,25 +14,20 @@ def main():
     if options.shortenRec:
         rec = shortenRec(rec=rec, timeDur = options.shortenRec)
 
-    unfiltered = downsample(rec, 30000) #temp for testing
-    save2mat(unfiltered, options.exportFolder + '/unfiltered.mat')#temp for testing
-    rec = bandpass(rec, options.desiredRate)
-    filtered = downsample(rec, 30000) #temp for testing
-    save2mat(filtered, options.exportFolder + '/filtered.mat')#temp for testing
-    downsampled = downsample(rec, options.desiredRate)
-    save2mat(downsampled, options.exportFolder + '/downsampled.mat')
-    
-    
+    save2mat(rec, options.exportFolder + '/unfiltered.mat')# TODO: remove temp for testing
+    rec = resample(rec,options.desiredRate)
+    save2mat(rec, options.exportFolder + '/downsampled.mat')
+     
 
-def save2mat(data, fname):
+def save2mat(rec, fname):
+    data = dict() # scipy.io.savemat converts dict to MATLAB struct
+    data['time_stamp'] = rec.get_times()
+    for id in rec.channel_ids:
+        trace = rec.get_traces(channel_ids=[id])
+        data[id] = np.squeeze(trace)
     scipy.io.savemat(fname, data, do_compression=True)
     return
-def save2csv(rec, fname):
-    # for x in data: #convert numpy arrays to lists before converting to dataframe
-    #     data[x] = data[x].tolist()
-    # table = pd.DataFrame.from_dict(data)
-    # table.to_csv(options.exportFolder + 'traces.csv')
-    return
+
 def parseInputs():
     parser = ArgumentParser(description='Spike sort a single recording')
     parser.add_argument('--dataFolder', 
@@ -84,23 +78,6 @@ def shortenRec(rec, timeDur):
     end = tstart[0]+timeDur
     rec = rec.time_slice(start_time=start,end_time=end)
     return rec
-def bandpass(rec, desiredRate):
-    freq_min=0.0000001 # Using a very low minimum frequency
-    freq_max = desiredRate * 0.5 # frequency cuttoff of 80% final sampling frequency. This number used by MATLAB decimate(). Nyquist means 50% sampling frequency is max, but filter isn't perfect.
-    return bandpass_filter(recording=rec,freq_min=freq_min,freq_max=freq_max)
-
-
-def downsample(rec, desiredRate):
-    data = dict()
-    decimation_factor = round(rec.get_sampling_frequency() / desiredRate)
-    t = rec.get_times()
-    data['time_stamp'] = t[ : : decimation_factor]
-
-    for id in rec.channel_ids: #break up by channel for less memory usage
-        trace = rec.get_traces(channel_ids=[id])
-        data[id] = trace[ : : decimation_factor]
-        data[id] = np.squeeze(data[id])
-    return data
 
 if __name__ == "__main__":
     main()
