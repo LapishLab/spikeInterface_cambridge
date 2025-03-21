06677b19fc2d011e921a893dfd1f7791df1d0eed
Lapishla IU HPC BigRed200 <davidswygart@gmail.com>
2025-03-20 20:39:01 -0400

    add state clearing flag

--- loadPhy.py
+++ loadPhy.py
@@ -1,13 +1,16 @@
 #!//N/u/lapishla/BigRed200/.conda/envs/phy2/bin/python
-import sys, os
-from phy.apps.template import template_gui
+import sys
+from os.path import isfile
 
 sorterOutput = sys.argv[1]
-
 dat_path = sorterOutput + '/temp_wh.dat'
 paramsPath = sorterOutput + '/params.py'
 newparamsPath = sorterOutput + '/params_update.py'
+
 def main():
+    if '--clear-state' in sys.argv:
+        clear_state()
+    from phy.apps.template import template_gui
     if validatePaths():
         updateParams()
         template_gui(newparamsPath)
@@ -15,11 +18,26 @@ def main():
         print('Skipping params.py update. Phy might be able to use raw data (does not seem to work for KS4 sorted data).')
         template_gui(paramsPath)
 
+def clear_state():
+    from pathlib import Path
+    global_state_file = str(Path.home()) + '/.phy/TemplateGUI/state.json'
+    local_state_file = sorterOutput + '/.phy/state.json'
+    if isfile(global_state_file):
+        print('Removing global state file')
+        # os.remove(global_state_file)
+    else:
+        print('Global state file not found in ', global_state_file)
+    if isfile(local_state_file):
+        print('Removing local state file')
+        # os.remove(local_state_file)
+    else:
+        print('Local state file not found ', local_state_file)
+
 def validatePaths():
     print('Validating paths')
-    if not os.path.isfile(paramsPath):
+    if not isfile(paramsPath):
         raise FileNotFoundError(f"params.py file not found at {paramsPath}")
-    if not os.path.isfile(dat_path):
+    if not isfile(dat_path):
         print(f"temp_wh.dat file not found at {dat_path}")
         return False
     return True
